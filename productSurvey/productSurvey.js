f9ff875672a4fa79152657ac644765dbae419137
Ligerwolfe <erunyan@me.com>
2025-01-26 18:50:16 -0600

    Update ProductSurvey

--- productSurvey/productSurvey.js
+++ productSurvey/productSurvey.js
@@ -1,30 +1,32 @@
-function submitFeedback(){
-     const username = document.getElementById("name").value;
-     const age = document.getElementById("age").value;
-     const email = document.getElementById("email").value;
-     const job = document.getElementById("job").value;
-     const designation = document.getElementById("designation").value;
-     const productType = document.getElementById("productType").value;
-     const feedback = document.getElementById("feedbackText").value;
-     const submitButton = document.getElementById("submitBtn");
+function submitFeedback() {
+const username = document.getElementById('name').value;
+const age = document.getElementById('age').value;
+const email = document.getElementById('email').value;
+const job = document.getElementById('job').value;
+const designation = document.getElementById('designation').value;
+const productType = document.getElementById('productType').value;
+const feedback = document.getElementById('feedbackText').value;
 
-     submitButton.onclick = submitFeedback;
-     alert("Thank you for your valuable feedback")
+const submitButton=document.getElementById('submitBtn');
 
-     document.getElementById("userName").innerHTML = username;
-     document.getElementById("userAge").innerHTML = age;
-     document.getElementById("userEmail").innerHTML = email;
-     document.getElementById("userJob").innerHTML = job;
-     document.getElementById("userDesignation").innerHTML = designation;
-     document.getElementById("userProductChoice").innerHTML = productType;
-     document.getElementById("userFeedback").innerHTML = feedback;
+submitButton.onclick = submitFeedback;
+alert('Thank you for your valuable feedback')
 
-     document.getElementById("userInfo").style.display = "block";
+document.getElementById('userName').innerHTML = username;
+document.getElementById('userAge').innerHTML = age;
+document.getElementById('userEmail').innerHTML = email;
+document.getElementById('userJob').innerHTML = job;
+document.getElementById('userDesignation').innerHTML = designation;
+document.getElementById('userProductChoice').innerHTML = productType;
+document.getElementById('userFeedback').innerHTML = feedback;
 
-     document.addEventListener('keydown', function(event) {
-          if(event.key === 'Enter'){
-               submitFeedback();
-          }
-     })
+document.getElementById('userInfo').style.display = 'block';
 
 }
+
+document.addEventListener('keydown', function(event) {
+     if (event.key === 'Enter') {
+       submitFeedback();
+     }
+   });
+
